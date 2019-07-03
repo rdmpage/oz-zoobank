014f473610aefcf65e22ccc5b28aa0e9effc4310
Roderic Page <rdmpage@gmail.com>
2019-07-03 14:21:41 +1000

    Add local OpenURL lookup

--- doi.php
+++ doi.php
@@ -41,4 +41,76 @@ function find_doi($string)
 			
 }
 
+//----------------------------------------------------------------------------------------
+function find_openurl($reference)
+{
+	$doi = '';
+	
+	$parameters = array();
+	
+	if (isset($reference->title) && ($reference->title != ''))
+	{
+		$atitle = $reference->title;
+		$atitle = strip_tags($atitle);
+
+		$parameters ['atitle'] = $atitle;
+	}
+	
+	
+	if (isset($reference->parentreference) && ($reference->parentreference != ''))
+	{
+		// clean
+		$title = $reference->parentreference;
+		$title = preg_replace('/,\s+\(.*\)$/u', '', $title);
+		
+		$parameters ['title'] = $title;
+	}
+
+	if (isset($reference->volume) && ($reference->volume != ''))
+	{
+		$parameters ['volume'] = $reference->volume;
+	}
+
+	if (isset($reference->startpage) && ($reference->startpage != ''))
+	{
+		$parameters ['spage'] = $reference->startpage;
+	}
+	
+	$url = 'http://localhost/~rpage/microcitation/www/api_openurl.php?' . http_build_query($parameters);
+	
+	//echo $url . "\n";	
+	
+	$opts = array(
+	  CURLOPT_URL =>$url,
+	  CURLOPT_FOLLOWLOCATION => TRUE,
+	  CURLOPT_RETURNTRANSFER => TRUE
+	);
+	
+	$ch = curl_init();
+	curl_setopt_array($ch, $opts);
+	$data = curl_exec($ch);
+	$info = curl_getinfo($ch); 
+	curl_close($ch);
+	
+	if ($data != '')
+	{
+		$obj = json_decode($data);
+		
+		//print_r($obj);
+		
+		if ($obj->found)
+		{
+			if (isset($obj->results[0]->doi))
+			{
+				$doi = $obj->results[0]->doi;
+			}
+		}
+		
+	}
+	
+	
+	return $doi;
+			
+}
+
 ?>
