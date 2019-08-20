a22ba9bc0184936af1a0ade746b1b0de1c311a36
Roderic Page <rdmpage@gmail.com>
2019-08-20 11:24:11 +1000

    RSS experiments

---
+++ rss.php
@@ -0,0 +1,80 @@
+<?php
+
+require_once('./utils.php');
+
+// get from RSS
+
+$rss = 'http://zoobank.org/rss/rss.xml';
+
+
+$url = 'https://grateful-crawdad.glitch.me/feed?q='
+	. urlencode($rss);
+	
+//echo $url . "\n";
+	
+$json = get($url);
+
+//echo $json;
+
+$obj = json_decode($json);
+
+//print_r($obj);
+
+$force = false;
+//$force = true;
+
+$basedir = dirname(__FILE__) . '/data';
+
+
+foreach ($obj->items as $item)
+{
+	if (preg_match('/:pub:/', $item->url))
+	{
+		$uuid = $item->id;
+
+		echo $uuid  . "\n";
+
+
+		$uuid = strtolower($uuid);
+	
+	
+		$uuid_path = create_path_from_sha1($uuid, $basedir);
+	
+		//echo $uuid_path  . "\n";
+	
+		$filename = $uuid_path . '/' . $uuid . '.json';
+	
+		echo $filename . "\n";
+	
+		$go = true;
+	
+		if (file_exists($filename) && !$force)
+		{
+			echo "File exists\n";
+			$go = false;
+		}
+	
+		if ($go)
+		{
+			$url = 'http://zoobank.org/References.json/' . strtolower($uuid);	
+			$json = get($url);	
+			
+			if ($json != '')
+			{
+			
+				// Remove array
+				$json = preg_replace('/^\[/u', '', $json);				
+				$json = preg_replace('/\]$/u', '', $json);
+			}				
+		}
+		
+		if ($json != '')
+		{
+			file_put_contents($filename, $json);
+		}
+	
+	}
+	
+}
+
+?>
